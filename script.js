05100866e16d131b10feb8bee5e05b3a4c4a0436
naja2008 <fathimanaja416@gmail.com>
2025-11-03 19:18:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const targetId = link.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+
+    window.scrollTo({
+      top: targetSection.offsetTop - 60,
+      behavior: 'smooth'
+    });
+  });
+});
